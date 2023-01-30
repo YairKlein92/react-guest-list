75319c77fda972151049011e0ab4b4d9089ee986
Csaba Klein <sympla@gmail.com>
2023-01-30 22:10:03 +0100

    Comment more out in buttons.js
    for more info see previous commit

--- src/buttons.js
+++ src/buttons.js
@@ -26,30 +26,30 @@
 //   setAttending(newListOfAttendingGuests);
 // };
 
-// old version - the logic is fine
-const showNonAttendingGuests = (event) => {
-  // 1. Click on this first
-  // event.preventDefault();
-  setGuests(guests); // 2. Trying to set the guests list back to its original, i.e. all guests full form
-  console.log(guests); // 3. logs the full list
-  const attendingGuests = [...guests];
-  console.log(attendingGuests); // 4. logs the full list
-  const newListOfNonAttendingGuests = attendingGuests.filter((guest) => {
-    return guest.attending === false;
-  });
-  console.log(newListOfNonAttendingGuests); // 5. logs the non-attending ppl
-  setGuests(newListOfNonAttendingGuests); // 6. changes the list -> only the non-attending people remain in the list
-};
+// // old version - the logic is fine
+// const showNonAttendingGuests = (event) => {
+//   // 1. Click on this first
+//   // event.preventDefault();
+//   setGuests(guests); // 2. Trying to set the guests list back to its original, i.e. all guests full form
+//   console.log(guests); // 3. logs the full list
+//   const attendingGuests = [...guests];
+//   console.log(attendingGuests); // 4. logs the full list
+//   const newListOfNonAttendingGuests = attendingGuests.filter((guest) => {
+//     return guest.attending === false;
+//   });
+//   console.log(newListOfNonAttendingGuests); // 5. logs the non-attending ppl
+//   setGuests(newListOfNonAttendingGuests); // 6. changes the list -> only the non-attending people remain in the list
+// };
 
-const showAttendingGuests = (event) => {
-  // event.preventDefault(); // same result without this line
-  setGuests(guests); // 7 Trying to set the guests list back to its original, i.e. all guests form
-  console.log(guests); // it shows a mutated list of non-attending ppl for some reason, even that it wasn't touched in the showNonAttendingGuests function
-  const newList = [...guests];
-  console.log(newList);
-  const newListed = newList.filter((guest) => {
-    return guest.attending === true;
-  });
-  console.log(newListed);
-  setGuests(newListed);
-};
+// const showAttendingGuests = (event) => {
+//   // event.preventDefault(); // same result without this line
+//   setGuests(guests); // 7 Trying to set the guests list back to its original, i.e. all guests form
+//   console.log(guests); // it shows a mutated list of non-attending ppl for some reason, even that it wasn't touched in the showNonAttendingGuests function
+//   const newList = [...guests];
+//   console.log(newList);
+//   const newListed = newList.filter((guest) => {
+//     return guest.attending === true;
+//   });
+//   console.log(newListed);
+//   setGuests(newListed);
+// };
